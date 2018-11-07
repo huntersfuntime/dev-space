094681c871e934947dda1f03d67b99650b2cafd9
hunterorges <hunterorges@gmail.com>
2018-11-07 15:09:11 -0700

    Integrated friendly routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
+
+  root to: 'pages#home'
 end
