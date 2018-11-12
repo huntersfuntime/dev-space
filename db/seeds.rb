9c790aee9a3f5b5a9dada2e88579b4cbfb2b5d4d
hunterorges <hunterorges@gmail.com>
2018-11-11 18:09:22 -0700

    Implement Devise for authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+puts "3 topics created"
+
 10.times do |blog|
     Blog.create!(
-        title: "Post #{blog}"
+        title: "Post #{blog}",
+        body: "Some stuff",
+        topic_id: Topic.last.id
     )
 end
 
